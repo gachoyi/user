f4e3f90ce60efd567ed0a4710bace55e36182da0
gachoyi <gachoyi@outlook.com>
2015-09-14 09:10:37 +0800

    more

--- test/test.go
+++ test/test.go
@@ -1,17 +1,9 @@
 package main
 import (
-    "fmt"
+	"fmt"
+	"math/rand"
 )
-var (
-    i uint64 = 1<<4-1
-)
+
 func main() {
-    d := []byte{'r', 'o', 'a', 'd'}
-    e := d[2:]
-    e[1] = 'm'
-    g := 2<<3
-    const f = "%T(%v)\n"
-    fmt.Printf(f,d,d)
-    fmt.Printf(f,e,e)
-    fmt.Println(g,i)
+	fmt.Println("my favorite number is", rand.Intn(10))
 }
